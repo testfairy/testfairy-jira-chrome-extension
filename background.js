589e57918f44cd7b8b2a863258b9dc9a72ffbab4
gmegidish <gil@megidish.net>
2016-09-22 15:00:47 +0300

    Support readyState == complete

--- background.js
+++ background.js
@@ -1,21 +1,15 @@
 chrome.extension.sendMessage({}, function(response) {
-
-	if (document.readyState === "interactive" && isJiraTab() ) {
-
+	if ((document.readyState === "interactive" || document.readyState === "complete") && isJiraTab()) {
 		window.addEventListener("message", receiveMessage, false);
-		
-		if (addTestfairyIFrame() == false) {
+		if (!addTestfairyIFrame()) {
 			setTimeout(addTestfairyIFrame, 5000);
 		}
-
 	}
 });
 
 function receiveMessage(event) {
-				
 	var origin = event.origin || event.originalEvent.origin; // For Chrome, the origin property is in the event.originalEvent object.
-	
-	if(event.data.height) {
+	if (event.data.height) {
 		document.getElementById('testfairy-iframe').height = (event.data.height) + "px";
 	}
 }
@@ -24,8 +18,8 @@ function addTestfairyIFrame() {
 
 	// Todo remove debug 
 	var a  = document.querySelector("#testfairy-session__testfairy-session-web-panel");
-	if (a != null ) {
-		//testfairy jira ad-on found
+	if (a != null  {
+		// testfairy jira add-on found
 		return true;
 	}
 
@@ -41,12 +35,10 @@ function addTestfairyIFrame() {
 
 	var url = testfairyDescriptionBlock.getAttribute("href");
 	if (!url.includes("projects") || !url.includes("builds") || !url.includes("sessions")) {
-
 		return false;
 	}
 	
 	if (url.includes('#')) {
-
 		url = url.substring(0, url.indexOf('#'));	
 	}
 	
@@ -81,18 +73,14 @@ function insertAfter(elem, refElem) {
 }
 
 function isJiraTab() {
-
 	var metaTags = document.getElementsByTagName("meta");
 
 	for (var i = 0; i < metaTags.length; i++) {
-    	
-    	if (metaTags[i].getAttribute('name') == 'application-name' && 
-    		metaTags[i].getAttribute('content') == 'JIRA' && 
-    		metaTags[i].getAttribute('data-name') == 'jira') {
-
-    		return true;
-    	}
+		if (metaTags[i].getAttribute('name') == 'application-name' && metaTags[i].getAttribute('content') == 'JIRA' && metaTags[i].getAttribute('data-name') == 'jira') {
+			return true;
+		}
 	}
 
 	return false;
 }
+
